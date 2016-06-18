893bdf75a608987c84036eec8faec25e824698f5
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-19 03:43:21 +0430

    Update pv.lua

--- plugins/pv.lua
+++ plugins/pv.lua
@@ -14,18 +14,17 @@ end
 function run(msg, matches)
 if msg.to.type == "user" and msg.text then
 fwd_msg("chat#id"..tonumber(fwd_to), msg.id,ok_cb,false)
-local text = '*msg : *_"..msg.text.."_\n*Sent To my Admin!*'
+return 'msg : "..msg.text.."\nSent To my Admin!'
 elseif msg.text and msg.reply_id and tonumber(msg.to.id) == fwd_to then
 if not msg.text then
-local text = "*You can only send Text message!*"
- send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+return "You can only send Text message!"
 end
 get_message(msg.reply_id, callback_message, msg)
 end
 end
 return {
 patterns = {
-"^(.*)$",
+"^/pm (.*)$",
 },
 run = run,
 }
