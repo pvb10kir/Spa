b6f04be22028ea1a941e86ab9132015b74bbbcb1
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-19 03:27:59 +0430

    Update pv.lua

--- plugins/pv.lua
+++ plugins/pv.lua
@@ -12,12 +12,12 @@ return nil
 end
 end
 function run(msg, matches)
-if msg.to.type == “user” and msg.text then
-fwd_msg(“chat#id”..tonumber(fwd_to), msg.id,ok_cb,false)
-local text = ‘*msg : *_"..msg.text.."_\n*Sent To my Admin!*’
+if msg.to.type == "user" and msg.text then
+fwd_msg("chat#id"..tonumber(fwd_to), msg.id,ok_cb,false)
+local text = '*msg : *_"..msg.text.."_\n*Sent To my Admin!*'
 elseif msg.text and msg.reply_id and tonumber(msg.to.id) == fwd_to then
 if not msg.text then
-local text = *“You can only send Text message!*”
+local text = "*You can only send Text message!*"
  send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 get_message(msg.reply_id, callback_message, msg)
@@ -25,7 +25,7 @@ end
 end
 return {
 patterns = {
-“^(.*)$”,
+"^/pm (.*)$",
 },
 run = run,
 }
