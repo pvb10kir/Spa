86596d494eccca2eb99366cbe415038b1dd1f729
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-20 03:24:00 +0430

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -1,18 +1,13 @@
-do function run(msg, matches)
-if matches[1]:lower() == '/start' then
-local text = "☑️*Hi "..msg.from.print_name.."* \n*Thx For Start Me*\n*You Can Use*\n_/help fa_\n_/help en_\n*For Show My Commands*\n[Good Luck](https://telegram.me/spheroch)"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
+local function run(msg)
+local text = [[*Hi Friend*
+*Thx For Start Me*
+*You Can Use*
+_/help fa_
+_/help en_
+*For Show My Commands*
+[Good Luck](https://telegram.me/spheroch)]]
+    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
-description = "show your id",
-usage = {
-moderator = {
-"!id : show your userid and information" },
-},
-patterns = {
-"^/(start)",
-},
-run = run
-}
-end
+ patterns = {"^/start$",},
+ run = run }
