89787dc5f90af36a6793ba86525d5c9bf873c1a0
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-20 03:35:37 +0430

    Create helpen.lua

---
+++ plugins/helpen.lua
@@ -0,0 +1,14 @@
+local function run(msg)
+local text = [[_Help Bot:_
+*Send Your Msg For Admin:*
+_/pm (msg)_
+*About @Sphero_Bot:*
+_/about_
+*Spherobot:*
+_/sphero_
+[Good Luck!](https://telegram.me/spheroch)]]
+    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
+return {
+ patterns = {"^help en$",},
+ run = run }
