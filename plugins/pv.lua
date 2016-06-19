6a4f113f53462a616c394f037c0290183494327d
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-20 03:56:08 +0430

    Create pv.lua

---
+++ plugins/pv.lua
@@ -0,0 +1,29 @@
+local fwd_to = 85831686
+
+local function callback_message(extra,success,result) 
+local receiver = result.to.id 
+local msg = extra 
+if result.fwd_from and msg.text then 
+fwd_msg(result.fwd_from.id, msg.id, ok_cb,false) 
+else 
+return nil 
+end 
+end 
+function run(msg, matches) 
+if msg.to.type == "user" and msg.text and not is_sudo(msg) then 
+fwd_msg("user#id"..tonumber(fwd_to), msg.id,ok_cb,false) 
+local text = '*📣Your msg Was Send To my Admin!*' 
+elseif msg.text and msg.reply_id and tonumber(msg.to.id) == fwd_to then 
+if not msg.text then 
+local text = "*You can only send Text message*" 
+ send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end 
+get_message(msg.reply_id, callback_message, msg) 
+end 
+end 
+return { 
+patterns = { 
+"^/pm (.*)$", 
+}, 
+run = run, 
+} 
