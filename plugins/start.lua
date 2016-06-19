2e62acb371c1b46d91f5b3518aef6f9d2ee3f8ae
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-20 00:23:10 +0430

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -1,11 +1,12 @@
 local function run(msg)
+if matches[1] "/start" then
 local text = [[*Hi My Friend*
 *Thanks For Start Me*
 *You Can Use*
 _/help fa_
 _/help en_
 *For See My Commands*]]
-    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+ send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
  patterns = {
