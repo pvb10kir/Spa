b57a2d303367d7dfd0dcac9fdd02ac92e6273187
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-20 16:14:04 +0430

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -1,11 +1,17 @@
 local function run(msg)
-local text = [[*Hi Friend*
+local text = [[*Hi My Friend*
 *Thx For Start Me*
 *You Can Use*
 _/help fa_
 _/help en_
 *For Show My Commands*
-[Good Luck](https://telegram.me/spheroch)]]
+[Good Luck](https://telegram.me/spheroch)
+`سلام دوست من
+تو میتونی از
+/help fa
+/help en
+برای دیدن دستورات من استفاده کنی `
+[موفق باشید](https://telegram.me/spheroch)]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
