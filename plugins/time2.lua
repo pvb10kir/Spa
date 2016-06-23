68eb85c27bfc2839bfcdc19a2c1c98cbc35bd60b
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-24 04:07:36 +0430

    Update time2.lua

--- plugins/time2.lua
+++ plugins/time2.lua
@@ -2,7 +2,7 @@ function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '*iran Time Now : *_'..jdat.FAtime..'_ \n*And Sticker Time👇*'
+local text = '*iran Time Now : *_'..jdat.ENtime..'_ \n*And Sticker Time👇*'
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
