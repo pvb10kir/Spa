bcc58b69bce4770278a3d620279bf7980ad1eb14
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-24 03:47:33 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -6,10 +6,6 @@ local url = "http://latex.codecogs.com/png.download?".."\\dpi{800}%20\\LARGE%20"
 local file = download_to_file(url,'time.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
 end
-if matches[1]:lower() == '/time' then
-local text = "*Iran Time Now*"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
 return {
   patterns = {
     "^/time",
