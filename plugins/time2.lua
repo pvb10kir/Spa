c54274c0371e8c2ba79e15d0fe60205f636d5752
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-24 03:51:28 +0430

    Create time2.lua

---
+++ plugins/time2.lua
@@ -0,0 +1,20 @@
+do function run(msg, matches)
+if matches[1]:lower() == '/time' then
+local text = "*Iran Time Now👆*"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
+end
+return {
+description = "show your id",
+usage = {
+moderator = {
+"!id : show your userid and information" },
+},
+patterns = {
+"^/time$",
+},
+run = run
+}
+end
+--Status API Training Shop Blog About
+-- 2016 GitHub, Inc. Terms Privacy Security Contact Help
