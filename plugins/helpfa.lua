3b051a9d5ae64f3434f79c4b05d7b4735a466da6
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-24 02:56:48 +0430

    Update helpfa.lua

--- plugins/helpfa.lua
+++ plugins/helpfa.lua
@@ -7,8 +7,8 @@ local text = [[`راهنمای ربات:
 اسفرو بات:
 /sphero
 متن به استیکر:
-/sticker[۱-۵] [متن] [سایز] [رنگ]
-/sticker[۱-۵ ][متن] [سایز]
+/sticker[۱-۵] [رنگ] [سایز] [متن]
+/sticker[۱-۵ ][سایز] [متن]
 /sticker[۱-۵] [متن]`
 [موفق باشید](https://telegram.me/spheroch)]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
