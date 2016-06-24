5fd9300bdb58116243a9b32ff04db2b5b86571f4
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-24 19:36:09 +0430

    Update chat3.lua

--- plugins/chat3.lua
+++ plugins/chat3.lua
@@ -1,16 +1,7 @@
-do function run(msg, matches)
-local text = "*Hi* ["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..") *WelCome to :* _"..msg.to.title.."_\n*Send Me* /start *For Help You*"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+local function run(msg)
+
+ send_api_msg(msg, get_receiver_api(msg), "*Hi* ["..msg.from.print_name.."](http://telegram.me/"..msg.from.username..") *WelCome to : *_"..msg.to.title.."_\n*Send Me* /start *For Help You*\n[#Good_Luck](https://telegram.me/spheroch)", true, 'md')
 end
 return {
-description = "show your id",
-usage = {
-moderator = {
-"!id : show your userid and information" },
-},
-patterns = {
-"^!!tgservice (chat_add_user_link)$",
-},
-run = run
-}
-end
+ patterns = {"!!tgservice (chat_add_user_link)",},
+ run = run }
