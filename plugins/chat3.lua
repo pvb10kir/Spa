c59561e47559d6cf6a74c006e811e537b2ce88cc
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-24 18:55:30 +0430

    Update chat3.lua

--- plugins/chat3.lua
+++ plugins/chat3.lua
@@ -1,5 +1,5 @@
 do function run(msg, matches)
-if matches[1]:lower() == 'chat_add_user_link' then
+if matches[1]:lower() == '!!tgservice (chat_add_user_link)' then
 local text = "*Hi* ["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..") WelCome to : _"..msg.to.title.."_\n*Send Me* /start *For Help You*"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
