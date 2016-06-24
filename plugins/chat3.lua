0f09e905f05c52f4aab50921e4696aa0fbf6cb38
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-24 19:21:03 +0430

    Update chat3.lua

--- plugins/chat3.lua
+++ plugins/chat3.lua
@@ -2,7 +2,6 @@ do function run(msg, matches)
 local text = "*Hi* ["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..") *WelCome to :* _"..msg.to.title.."_\n*Send Me* /start *For Help You*"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
-end
 return {
 description = "show your id",
 usage = {
