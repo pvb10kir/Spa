87b88d9a5175dd9fc3265f7ba32470a862625b9e
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-29 04:33:15 +0430

    Update myinfo.lua

--- plugins/myinfo.lua
+++ plugins/myinfo.lua
@@ -1,5 +1,5 @@
 local function run(msg)
- send_api_msg(msg, get_receiver_api(msg), "*FullName :* `"..msg.from.print_name.."`\n*Your ID : *`"..msg.to.id.."`\n*Your Msg ID :* `"..msg.id.."`\n[GoodLuck!](https://telegram.me/spheroch)", true, 'md')
+ send_api_msg(msg, get_receiver_api(msg), "*FullName :* `"..msg.from.print_name.."`\n*Your ID : *`"..msg.to.id.."`\n*Username : *[@"..msg.from.username.."](https://telegram.me/"..msg.from.username..")\n*Your Msg ID :* `"..msg.id.."`\n[GoodLuck!](https://telegram.me/spheroch)", true, 'md')
 end
 return {
  patterns = {"/myinfo",},
