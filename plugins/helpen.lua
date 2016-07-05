3ffeaf1a429fc2160b2de6fd1d42c5ef21908fd1
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-07-05 19:49:30 +0430

    Update helpen.lua

--- plugins/helpen.lua
+++ plugins/helpen.lua
@@ -1,23 +1,27 @@
 local function run(msg)
-local text = [[_Help Bot:_
-*Send Your Msg For Admin:*
+local text = [[_Help Bot :_
+*Send Your Msg For Admin :*
 _/pm (msg)_
-*About @Sphero_Bot:*
+*About @Sphero_Bot :*
 _/about_
-*Spherobot:*
+*Spherobot :*
 _/sphero_
-*Text To Sticker:*
+*Text To Sticker :*
 _/sticker[1-5] [text] [size] [color]_
 _/sticker[1-5] [text] [size]_
 _/sticker[1-5] [text]_
-*Show iran Time:*
+*Show iran Time :*
 _/time_
-*Text to Qrcode:*
+*Text to Qrcode :*
 _/qrcode [text]_
+*Show En/Fa Date And Time :*
 _/dayinfo_
-*Show En/Fa Date And Time*
+*Return Your Info :*
 _/myinfo_
-*Return Your Info*
+*Sticker To Photo :*
+_/photo (just reply your sticker)_
+*Photo To Sticker :*
+_/sticker (just reply your photo)_
 [Good Luck!](https://telegram.me/spheroch)]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
