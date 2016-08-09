9063f5920240f2c301aa9c576e33fc8af1b5f1da
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-10 00:46:33 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {85831686},--Sudo users
+    sudo_users = {255317894},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
